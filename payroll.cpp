4491cd6dd512aed78305de0bd35d7013125c2409
eyuroot <ethioroot00@gmail.com>
2022-03-29 20:14:09 +0300

    last parolle programm

--- payroll.cpp
+++ payroll.cpp
@@ -9,6 +9,8 @@ float calPension(float);
 int incScale(float);
 void listData();
 void addEmplo();
+void delUser();
+void modifyData();
 void menu();
 void wait();
 void showTable();
@@ -21,7 +23,7 @@ struct employee
 {
     string name;
     float salary;
-    //float bonus;
+    float bonus;
 };
 
 struct employeeInfo{
@@ -29,7 +31,7 @@ struct employeeInfo{
     float pension;
     float netIncome;
 };
-
+//the program store data using array
 employee employeeData[10]; // employe data holder entered by the user
 employeeInfo employeeCalInf[10]; // employee calculated information
 int noEmplo=0; //number of employee used to track the data entered by the user
@@ -40,20 +42,61 @@ int main(){
     return 0;
 }
 
+void menu(){
+    int menuIndex;
+    bool waited = true;
+    cout << "Payroll Management System"<<endl;
+    cout << "1. Add an employee."<<endl;
+    cout << "2. List employee data."<<endl;
+    cout << "3. To modify data."<<endl;
+    cout << "4. To delet an employee."<<endl;
+    cout << "0. Exit"<<endl;
+    cout << "Please enter your choice: ";
+    cin >> menuIndex;
+    switch (menuIndex)
+    {
+    case 1:
+        addEmplo();
+        break;
+    case 2:
+        showTable();
+       // waited = false;
+        //wait();
+        break;
+    case 3:
+        modifyData();
+        break;
+    case 4:
+        delUser();
+        break;
+    case 0:
+        exit(0);
+        break;
+    default:
+        system("cls");
+        cout << "Please choose from the above"<< endl;
+        menu();
+    }
+    if (waited){
+        wait();
+    }
+    menu();
+}
+//calculate Income tax
 float calInTax(float salary){
     int scale = incScale(salary);
     float incomeTax = (salary * taxRate[scale]) - deduction[scale];
     return incomeTax;
 }
-
+//calculate pension
 float calPension(float salary){
     return salary * 0.07;
 }
-
+//calculate net income
 float calNetIn(float salary){
     return salary - calInTax(salary) - calPension(salary);
 }
-
+// calculate income scale there are different salary scale to tax rate
 int incScale(float salary){
     int scale;
     if(salary > 0 && salary <= 600)
@@ -74,56 +117,24 @@ int incScale(float salary){
 }
 
 void addEmplo(){
-    cout << "Enter your employee"<< noEmplo+1 <<" name: ";
+    cout << "Enter your employee "<< noEmplo+1 <<" name: ";
     cin.ignore('\n',10);
     getline(cin,employeeData[noEmplo].name,'\n');
-    cout << "Enter your employee"<< noEmplo+1 <<" salary: ";
+    cout << "Enter your employee "<< noEmplo+1 <<" salary: ";
     cin >> employeeData[noEmplo].salary;
-    // cout << "Enter your employee"<< noEmplo+1 <<" bonus: ";
-    // cin >> employeeData[noEmplo].bonus;
+    cout << "Enter your employee "<< noEmplo+1 <<" bonus: ";
+    cin >> employeeData[noEmplo].bonus;
+    //calculate user information
     employeeCalInf[noEmplo].incomeTax = calInTax(employeeData[noEmplo].salary);  //calculate user information
     employeeCalInf[noEmplo].pension = calPension(employeeData[noEmplo].salary);
-    employeeCalInf[noEmplo].netIncome = calNetIn(employeeData[noEmplo].salary);
+    employeeCalInf[noEmplo].netIncome = calNetIn(employeeData[noEmplo].salary) + employeeData[noEmplo].bonus;
 
     noEmplo++; // add 1 to the next index in employee data array
 }
 
-void menu(){
-    int menuIndex;
-    bool waited = true;
-    cout << "Payroll Management System :)"<<endl;
-    cout << "1. Add employee."<<endl;
-    cout << "2. List employee data."<<endl;
-    cout << "0. Exit"<<endl;
-    cout << "Please enter your choice: ";
-    cin >> menuIndex;
-    switch (menuIndex)
-    {
-    case 1:
-        addEmplo();
-        break;
-    case 2:
-        showTable();
-        waited = false;
-        wait();
-        break;
-    case 0:
-        exit(0);
-        break;
-    default:
-        system("cls");
-        cout << "Please choose from the above"<< endl;
-        menu();
-    }
-    if (waited){
-        wait();
-    }
-    menu();
-}
-
 void wait(){
     cout << "(please enter to continue or press any key to exit)";
-    cin.ignore(100,'\n');
+    cin.ignore(10,'\n');
     if (cin.get() == '\n') {
         system("cls");
     }
@@ -131,7 +142,7 @@ void wait(){
         exit(0);
     }
 }
-
+//this function show a user information using table
 void showTable(){
     TextTable t( '-', '|', '+' );
 
@@ -139,6 +150,7 @@ void showTable(){
     t.add( "Salary" );
     t.add( "Income Tax" );
     t.add( "Pension" );
+    t.add( "Bonus" );
     t.add( "Net salary" );
     t.endOfRow();
 
@@ -148,6 +160,7 @@ void showTable(){
         t.add(Convert(employeeData[i].salary));
         t.add(Convert(employeeCalInf[i].incomeTax));
         t.add(Convert(employeeCalInf[i].pension));
+        t.add(Convert(employeeData[i].bonus));
         t.add(Convert(employeeCalInf[i].netIncome));
         t.endOfRow();   
     }
@@ -160,4 +173,59 @@ string Convert(float number){
     ostringstream buff;
     buff<<fixed<<setprecision(2)<<number;//setting the precision to two
     return buff.str();//converting to string
+}
+
+void modifyData(){
+    int emploList,dataList;
+    cout << "Modify entered data." << endl;
+    for(int i=0;i < noEmplo;i++){
+        cout << i+1<< ". "<< employeeData[i].name << endl;
+    }
+    cout << "Enter the user number you want to modify: ";
+    cin >> emploList;
+    cout << "1. " << employeeData[emploList-1].name<<endl;
+    cout << "2. " << employeeData[emploList-1].salary<<endl;
+    cout << "3. " << employeeData[emploList-1].bonus<<endl;
+    cout << "Enter the data number you want to modify(enter 0 to go back): ";
+    cin >> dataList;
+    if(dataList == 1){
+        string newName;
+        cout << "Enter new name: ";
+        cin.ignore(10,'\n');
+        getline(cin, newName);
+        employeeData[emploList-1].name = newName;
+    }else if(dataList == 2){
+        float newSalary;
+        cout << "Enter new salary: ";
+        cin >> newSalary;
+        employeeData[emploList-1].salary = newSalary;
+    }else if(dataList == 3){
+        float newBonus;
+        cout << "Enter new bonus: ";
+        cin >> newBonus;
+        employeeData[emploList-1].bonus = newBonus;
+    }
+}
+
+void delUser(){
+    int delEmplo;
+    cout << "Delete an employee data." << endl;
+    for(int i=0;i < noEmplo;i++){
+        cout << i+1<< ". "<< employeeData[i].name << endl;
+    }
+    cout << "Enter the user number you want to delet(0 to go back): ";
+    cin >> delEmplo; // check wheather the user want continue
+    if(delEmplo > 0){
+        delEmplo--;
+        for(int i = delEmplo-1;i < 6;i++){
+            employeeData[delEmplo].name = employeeData[delEmplo + 1].name;
+            employeeData[delEmplo].salary = employeeData[delEmplo + 1].salary;
+            employeeData[delEmplo].bonus = employeeData[delEmplo + 1].bonus;
+            employeeCalInf[delEmplo].pension = employeeCalInf[delEmplo + 1].pension;
+            employeeCalInf[delEmplo].incomeTax = employeeCalInf[delEmplo + 1].incomeTax;
+            employeeCalInf[delEmplo].netIncome = employeeCalInf[delEmplo + 1].netIncome;
+            delEmplo++;
+        }
+        noEmplo--; //an employee decrease by 1
+    }
 }
